8de029ea1f8f2c6293025f14243c52201e03de66
Ivan Ivanov <vankataloz@gmail.com>
2023-01-26 23:05:19 +0200

    change

--- lection3/smallShop.js
+++ lection3/smallShop.js
@@ -1,4 +1,68 @@
 function smallShop(input){
 
+    let product = input[0];
+    let city = input[1];
+    let quantity = Number(input[2]);
+    let totalSum = 0;
+    switch(city){
+        case "Sofia":
+            switch(product){
+                case "coffee":
+                    totalSum = quantity * 0.50;
+                    break;
+                case "water":
+                    totalSum = quantity * 0.80;
+                        break;
+                case "beer":
+                    totalSum = quantity * 1.20;
+                        break;
+                case "sweets":
+                    totalSum = quantity * 1.45;
+                        break;
+                case "peanuts":
+                    totalSum = quantity * 1.60;
+                        break;
+            }
+            break;
+        case "Plovdiv":
+            switch(product){
+                case "coffee":
+                    totalSum = quantity * 0.40;
+                    break;
+                case "water":
+                    totalSum = quantity * 0.70;
+                    break;
+                case "beer":
+                    totalSum = quantity * 1.15;
+                    break;
+                case "sweets":
+                    totalSum = quantity * 1.30;
+                    break;
+                case "peanuts":
+                    totalSum = quantity * 1.50;
+                    break;
+            }
+            break;
+        case "Varna":
+            switch(product){
+                case "coffee":
+                    totalSum = quantity * 0.45;
+                    break;
+                case "water":
+                    totalSum = quantity * 0.70;
+                    break;
+                case "beer":
+                    totalSum = quantity * 1.10;
+                    break;
+                case "sweets":
+                    totalSum = quantity * 1.35;
+                    break;
+                case "peanuts":
+                    totalSum = quantity * 1.55;
+                    break;
+            }
+            break;
+    }
+    console.log(totalSum);
 }
-smallShop([]);
+smallShop(["sweets", "Sofia", "2.23"]);
